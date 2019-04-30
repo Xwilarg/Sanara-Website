177f7904817d6507dc692ee46c8010f2d80cb05a
Xwilarg <xwilarg@yahoo.fr>
2019-04-30 15:21:12 +0200

    Clean buttons

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 document.getElementById("menu").innerHTML =
 '<ul>\
-  <li><a href="index.html">Home</a></li>\
+  <li> <a href="https://github.com/Xwilarg/Sanara"><img alt="GitHub" src="img/GitHub.png"></a></li>\
 </ul>\
 ';
