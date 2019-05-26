e29d7d9dec1fe392a4137dabd5f7634d36b1e091
Xwilarg <xwilarg@yahoo.fr>
2019-05-26 12:47:46 +0200

    Add info update on index.html

--- js/index.js
+++ js/index.js
@@ -1,18 +1,23 @@
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        response = JSON.parse(this.responseText);
-        document.getElementById("cmdLaunched").innerHTML = response.commandsLastMonth;
-        document.getElementById("serverCount").innerHTML = response.serverCount;
+function updateInfos() {
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            response = JSON.parse(this.responseText);
+            document.getElementById("cmdLaunched").innerHTML = response.commandsLastMonth;
+            document.getElementById("serverCount").innerHTML = response.serverCount;
+        }
     }
-}
-xmlhttp.open("GET", "https://api.zirk.eu/bots.php?name=Sanara&small=true", true);
-xmlhttp.send();
-let xmlhttp2 = new XMLHttpRequest();
-xmlhttp2.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("githubStars").innerHTML = JSON.parse(this.responseText).stargazers_count;
+    xmlhttp.open("GET", "https://api.zirk.eu/bots.php?name=Sanara&small=true", true);
+    xmlhttp.send();
+    let xmlhttp2 = new XMLHttpRequest();
+    xmlhttp2.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("githubStars").innerHTML = JSON.parse(this.responseText).stargazers_count;
+        }
     }
+    xmlhttp2.open("GET", "https://api.github.com/repos/xwilarg/sanara", true);
+    xmlhttp2.send();
 }
-xmlhttp2.open("GET", "https://api.github.com/repos/xwilarg/sanara", true);
-xmlhttp2.send();
+
+updateInfos();
+window.setInterval(updateInfos, 60000); // Every minute
