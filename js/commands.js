3911ed46b5fb586dcef3dd821e979e29173a1ca1
Xwilarg <xwilarg@yahoo.fr>
2020-09-27 16:25:03 +0200

    Update website for V3

--- js/commands.js
+++ js/commands.js
@@ -1,3 +1,15 @@
+function displayPage(name) {
+    document.getElementById("administration").hidden = true;
+    document.getElementById("entertainment").hidden = true;
+    document.getElementById("game").hidden = true;
+    document.getElementById("nsfw").hidden = true;
+    document.getElementById("radio").hidden = true;
+    document.getElementById("tool").hidden = true;
+    document.getElementById("table").hidden = true;
+
+    document.getElementById(name).hidden = false;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     if (window.location.href.includes('?')) {
         let parsing = new URLSearchParams(window.location.href.split('?')[1]);
@@ -6,3 +18,27 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+
+function displayAdministration() {
+    displayPage("administration");
+}
+
+function displayEntertainment() {
+    displayPage("entertainment")
+}
+
+function displayGame() {
+    displayPage("game");
+}
+
+function displayNSFW() {
+    displayPage("nsfw");
+}
+
+function displayRadio() {
+    displayPage("radio");
+}
+
+function displayTool() {
+    displayPage("tool");
+}
