fd6205dec34cd11838326f665fb285c3d80c53f1
Xwilarg <xwilarg@yahoo.fr>
2020-11-07 18:17:56 +0100

    Generate help from JSON

--- js/commands.js
+++ js/commands.js
@@ -1,10 +1,11 @@
 function displayPage(name) {
-    document.getElementById("administration").hidden = true;
-    document.getElementById("entertainment").hidden = true;
-    document.getElementById("game").hidden = true;
-    document.getElementById("nsfw").hidden = true;
-    document.getElementById("radio").hidden = true;
-    document.getElementById("tool").hidden = true;
+    document.getElementById("tableHelp").hidden = false;
+    document.getElementById("Administration").hidden = true;
+    document.getElementById("Entertainment").hidden = true;
+    document.getElementById("Game").hidden = true;
+    document.getElementById("Nsfw").hidden = true;
+    document.getElementById("Radio").hidden = true;
+    document.getElementById("Tool").hidden = true;
     document.getElementById("table").hidden = true;
 
     document.getElementById(name).hidden = false;
@@ -20,25 +21,53 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function displayAdministration() {
-    displayPage("administration");
+    displayPage("Administration");
 }
 
 function displayEntertainment() {
-    displayPage("entertainment")
+    displayPage("Entertainment")
 }
 
 function displayGame() {
-    displayPage("game");
+    displayPage("Game");
 }
 
 function displayNSFW() {
-    displayPage("nsfw");
+    displayPage("Nsfw");
 }
 
 function displayRadio() {
-    displayPage("radio");
+    displayPage("Radio");
 }
 
 function displayTool() {
-    displayPage("tool");
+    displayPage("Tool");
 }
+
+let http = new XMLHttpRequest();
+http.open("GET", "php/getJson.php?file=Help", false);
+http.onreadystatechange = function ()
+{
+    if (this.readyState === 4 && this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        for (j in json) {
+            var elem = json[j];
+            var c = elem.Item2;
+            let args = "";
+            for (index in c.Arguments) {
+                let arg = c.Arguments[index];
+                if (arg.Type == 0) args += "(" + arg.Content + ") ";
+                else args += "[" + arg.Content + "] ";
+            }
+            document.getElementById(elem.Item1).innerHTML +=
+            '<tr>' +
+            `<td>${c.CommandName}</td>` +
+            `<td>${args}</td>` +
+            `<td>${c.Description}</td>` +
+            `<td>${c.Restriction}</td>` +
+            `<td>${c.SubmoduleName}</td>` +
+            '</tr>';
+        }
+    }
+};
+http.send(null);
---
+++ php/getJson.php
@@ -0,0 +1,2 @@
+<?php
+echo file_get_contents(__DIR__ . "/../json/" . $_GET['file'] . ".json");
