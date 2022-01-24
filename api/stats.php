7212db2581a7ddecce2bf2de2e437fd9d1b718e8
Xwilarg <xwilarg@yahoo.fr>
2022-01-24 20:26:53 +0100

    Improve API

--- api/stats.php
+++ api/stats.php
@@ -1,28 +1,34 @@
 <?php
 require './vendor/autoload.php';
-header('Content-Type: application/json');
 error_reporting(0);
+header('Content-Type: application/json');
 $conn = r\connect('localhost');
 $now = new DateTime();
-$date = $now->format("Ymd");
 
 function remove_id($array) {
     return array_diff_key(json_decode(json_encode($array), true), array_flip(array("id"))); 
 }
 
-$commands = array();
-$nb_messages = array();
-for ($i = 0; $i < 10; $i += 1) {
-    $curr = new DateTime();
-    $curr->sub(new DateInterval("PT" . strval($i) . "H"));
-    array_push($commands, remove_id(r\db('Hanaki_stats')->table('Commands')->get($curr->format("YmdH"))->run($conn)));
+function getStats($name, $conn, $now) {
+    $date = $now->format("Ymd");
+
+    $commands = array();
+    $nb_messages = array();
+    for ($i = 0; $i < 10; $i += 1) {
+        $curr = new DateTime();
+        $curr->sub(new DateInterval("PT" . strval($i) . "H"));
+        array_push($commands, remove_id(r\db($name)->table('Commands')->get($curr->format("YmdH"))->run($conn)));
+    }
+
+    return array(
+        "guild_count"   => remove_id(r\db($name)->table('GuildCount')->get($date)->run($conn)),
+        "errors"        => remove_id(r\db($name)->table('Errors')->get($date)->run($conn)),
+        "commands"      => $commands,
+        "games"         => remove_id(r\db($name)->table('Games')->get($date)->run($conn)),
+        "games_players" => remove_id(r\db($name)->table('GamesPlayers')->get($date)->run($conn)),
+        "booru"         => remove_id(r\db($name)->table('Booru')->get($date)->run($conn)),
+        "download"      => remove_id(r\db($name)->table('Download')->get($date)->run($conn))
+    );
 }
 
-echo(json_encode(array(
-    "guild_count"   => remove_id(r\db('Hanaki_stats')->table('GuildCount')->get($date)->run($conn)),
-    "errors"        => remove_id(r\db('Hanaki_stats')->table('Errors')->get($date)->run($conn)),
-    "commands"      => $commands,
-    "games"         => remove_id(r\db('Hanaki_stats')->table('Games')->get($date)->run($conn)),
-    "games_players" => remove_id(r\db('Hanaki_stats')->table('GamesPlayers')->get($date)->run($conn)),
-    "booru"         => remove_id(r\db('Hanaki_stats')->table('Booru')->get($date)->run($conn))
-)));
+echo(json_encode(getStats("Hanaki_stats", $conn, $now)));
