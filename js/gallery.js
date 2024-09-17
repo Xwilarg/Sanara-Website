471c87d7b82a622c20e76e9e7a9386e913394f7d
Xwilarg <xwilarg@protonmail.com>
2024-09-18 00:32:41 +0100

    Improve gallery

--- js/gallery.js
+++ js/gallery.js
@@ -3,12 +3,20 @@ function get2DigitsNumber(nb) {
     else return nb;
 }
 
-function showPopup(name, source, imageGroup, nbImages) {
+function showPopup(name, source, imageGroup, nbImages, nsfwStarting, ext) {
+    if (source === null) popupSource.classList.add("hidden")
+    else popupSource.classList.remove("hidden")
+
     document.getElementById("popup").classList.remove("hidden")
     document.getElementById("popupAuthorName").innerHTML = name;
     let finalHtml = "";
     for (let i = 1; i <= nbImages; i++) {
-        finalHtml += "<img src='img/Gallery/" + imageGroup + "_" + get2DigitsNumber(i) + ".jpg'/><br/>"
+        finalHtml += "<img"
+        if (nsfwStarting !== undefined && i >= nsfwStarting)
+        {
+            finalHtml += ' class="nsfw"'
+        }
+        finalHtml += " src='gallery/" + imageGroup + "_" + get2DigitsNumber(i) + "." + ext + "'/><br/>"
     }
     document.getElementById("popupImages").innerHTML = finalHtml;
     document.getElementById("popupSource").href = source;
