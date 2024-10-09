c8c13f10dbb1c787de82b233713c71fbefa4b830
Xwilarg <xwilarg@protonmail.com>
2024-10-09 14:21:55 +0200

    Update gallery

--- js/gallery.js
+++ js/gallery.js
@@ -3,12 +3,12 @@ function get2DigitsNumber(nb) {
     else return nb;
 }
 
-function showPopup(name, source, imageGroup, nbImages, nsfwStarting, ext) {
+function showPopup(name, authorUrl, source, imageGroup, nbImages, nsfwStarting, ext) {
     if (source === null) popupSource.classList.add("hidden")
     else popupSource.classList.remove("hidden")
 
     document.getElementById("popup").classList.remove("hidden")
-    document.getElementById("popupAuthorName").innerHTML = name;
+    document.getElementById("popupAuthorName").innerHTML = `<a href="${authorUrl}" target="_blank">${name}</a>`;
     let finalHtml = "";
     for (let i = 1; i <= nbImages; i++) {
         finalHtml += "<img"
@@ -19,5 +19,7 @@ function showPopup(name, source, imageGroup, nbImages, nsfwStarting, ext) {
         finalHtml += " src='gallery/" + imageGroup + "_" + get2DigitsNumber(i) + "." + ext + "'/><br/>"
     }
     document.getElementById("popupImages").innerHTML = finalHtml;
+    if (source === undefined) document.getElementById("popupSource").classList.add("hidden");
+    else document.getElementById("popupSource").classList.remove("hidden");
     document.getElementById("popupSource").href = source;
 }
